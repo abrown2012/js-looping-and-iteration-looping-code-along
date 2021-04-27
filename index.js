35f08ba9c45023a5fdda9d57780b0241f136d16c
Agnieszka Brown <abrown2012@icloud.com>
2021-04-27 00:06:47 -0400

    commit tree d9a22ff1444fc26314cbc57aa835e53027c28e73

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(gifts, type) {
+    let new_array = [];
+    for (let i = 0; i < gifts.length; i++) {
+      new_array.push(`Thank you, ${gifts[i]}, for the wonderful ${type} gift!`);
+    }
+    return new_array;
+  }
+
+function countDown(number) {
+    for (let i = number; i >= 0 ; i --) {
+        console.log(i)
+    }
+}
